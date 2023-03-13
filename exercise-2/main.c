5f862d692533c398bea66aa6d156ef0deb7f0f67
LeonKattendick <leon.kattendick@gmail.com>
2023-03-13 23:03:02 +0100

    testing

--- exercise-2/main.c
+++ exercise-2/main.c
@@ -15,6 +15,14 @@ float transformX(float px, int width);
 
 float transformY(float py, int height);
 
+void runParallelMandelbrotTest(int width, int height, unsigned char *img, int numberOfThreads, int iterations);
+
+void runSequentialMandelbrotTest(int width, int height, unsigned char *img, int iterations);
+
+double parallelMandelbrotExecution(int width, int height, unsigned char *img);
+
+double sequentialMandelbrotExecution(int width, int height, unsigned char *img);
+
 int MAX_ITERATIONS = 100;
 
 float X_MIN = -2.0f;
@@ -35,10 +43,47 @@ int main(int argc, char **argv) {
 
     unsigned char *img = malloc(width * height * 3);
 
+    runSequentialMandelbrotTest(width, height, img, 10);
+
+    for (int i = 2; i <= omp_get_num_procs(); i++) {
+        runParallelMandelbrotTest(width, height, img, i, 10);
+    }
+
+    stbi_write_jpg("../out/test.jpg", width, height, 3, img, 100);
+    free(img);
+
+    return 0;
+}
+
+void runParallelMandelbrotTest(int width, int height, unsigned char *img, int numberOfThreads, int iterations) {
+
+    omp_set_num_threads(numberOfThreads);
+
+    double sum = 0.0;
+    for (int i = 0; i < iterations; i++) {
+        sum += parallelMandelbrotExecution(width, height, img);
+    }
+
+    printf("Parallel run (threads: %02d, iterations: %d) took %.3f seconds.\n", numberOfThreads, iterations,
+           sum / iterations);
+}
+
+void runSequentialMandelbrotTest(int width, int height, unsigned char *img, int iterations) {
+
+    double sum = 0.0;
+    for (int i = 0; i < iterations; i++) {
+        sum += sequentialMandelbrotExecution(width, height, img);
+    }
+
+    printf("Sequential run (iterations: %d) took %.3f seconds.\n", iterations, sum / iterations);
+}
+
+double parallelMandelbrotExecution(int width, int height, unsigned char *img) {
+
     clock_t begin = clock();
 
-    for (int x = 0; x < width; x++) {
 #pragma omp parallel for
+    for (int x = 0; x < width; x++) {
         for (int y = 0; y < height; y++) {
             unsigned char *pixel = img + (x + width * y) * 3;
             calculatePixel((float) x, (float) y, pixel, width, height);
@@ -46,12 +91,22 @@ int main(int argc, char **argv) {
     }
 
     clock_t end = clock();
-    printf("Image generation took %.3f seconds.", (double) (end - begin) / CLOCKS_PER_SEC);
+    return (double) (end - begin) / CLOCKS_PER_SEC;
+}
 
-    stbi_write_jpg("../out/test.jpg", width, height, 3, img, 100);
-    free(img);
+double sequentialMandelbrotExecution(int width, int height, unsigned char *img) {
 
-    return 0;
+    clock_t begin = clock();
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            unsigned char *pixel = img + (x + width * y) * 3;
+            calculatePixel((float) x, (float) y, pixel, width, height);
+        }
+    }
+
+    clock_t end = clock();
+    return (double) (end - begin) / CLOCKS_PER_SEC;
 }
 
 void calculatePixel(float px, float py, unsigned char *pixel, int width, int height) {
@@ -76,9 +131,15 @@ void calculatePixel(float px, float py, unsigned char *pixel, int width, int hei
 }
 
 void coolPixelColoring(unsigned char *pixel, int n) {
-    pixel[0] = n;
-    pixel[1] = n;
-    pixel[2] = n;
+    if (n == 0) {
+        pixel[0] = 0;
+        pixel[1] = 250;
+        pixel[2] = 255;
+        return;
+    }
+    pixel[0] = 0;
+    pixel[1] = 250 * n;
+    pixel[2] = 255 * n;
 }
 
 float transformX(float px, int width) {
