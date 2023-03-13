b929880c09bb4b5aed80863b43cc7a8a2a5c710b
LeonKattendick <leon.kattendick@gmail.com>
2023-03-13 21:52:00 +0100

    removed unused

--- exercise-2/main.c
+++ exercise-2/main.c
@@ -3,10 +3,6 @@
 #include <omp.h>
 #include <time.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-
-#include "stb_image/stb_image.h"
-
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include "stb_image/stb_image_write.h"
