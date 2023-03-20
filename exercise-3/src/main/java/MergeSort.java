15f3d1c6b804c57590745dce176644c3f7490722
dorrlixD1 <dorr.dominik@protonmail.com>
2023-03-20 13:47:35 +0100

    MergeSort implemented

--- exercise-3/src/main/java/MergeSort.java
+++ exercise-3/src/main/java/MergeSort.java
@@ -1,3 +1,7 @@
+import com.sun.source.tree.NewArrayTree;
+
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
@@ -7,29 +11,65 @@ public class MergeSort {
 
     private final ExecutorService executorService = Executors.newFixedThreadPool(16);
 
-    private final int length;
-
     private final int threshold;
 
-    private final int[] elements;
-
-    public MergeSort(int length, int threshold) {
-        this.length = length;
+    public MergeSort(int threshold) {
         this.threshold = threshold;
-        this.elements = new int[length];
+    }
 
-        Random r = new Random();
-        for (int i = 0; i < length; i++) {
-            this.elements[i] = r.nextInt(length * 1000);
+    public int[] mergeSort(int[] elements, int left, int right) {
+        // End of recursion reached?
+        if (left == right) return new int[]{elements[left]};
+
+        int middle = left + (right - left) / 2;
+        int[] leftArray = mergeSort(elements, left, middle);
+        int[] rightArray = mergeSort(elements, middle + 1, right);
+        return merge(leftArray, rightArray);
+    }
+
+    private int[] merge(int[] leftArray, int[] rightArray) {
+        int leftLen = leftArray.length;
+        int rightLen = rightArray.length;
+
+        int[] target = new int[leftLen + rightLen];
+        int targetPos = 0;
+        int leftPos = 0;
+        int rightPos = 0;
+
+        // As long as both arrays contain elements...
+        while (leftPos < leftLen && rightPos < rightLen) {
+            // Which one is smaller?
+            int leftValue = leftArray[leftPos];
+            int rightValue = rightArray[rightPos];
+            if (leftValue <= rightValue) {
+                target[targetPos++] = leftValue;
+                leftPos++;
+            } else {
+                target[targetPos++] = rightValue;
+                rightPos++;
+            }
         }
+        // Copy the rest
+        while (leftPos < leftLen) {
+            target[targetPos++] = leftArray[leftPos++];
+        }
+        while (rightPos < rightLen) {
+            target[targetPos++] = rightArray[rightPos++];
+        }
+        return target;
     }
 
-    public int[] getElements() {
-        return elements;
-    }
+    static class Stopwatch {
 
-    public void sort() {
+        private long startTime;
 
+        public void start() {
+            this.startTime = System.currentTimeMillis();
+        }
+
+        public long stop() {
+            return System.currentTimeMillis() - this.startTime;
+        }
     }
 
     public static void main(String[] args) {
@@ -40,15 +80,26 @@ public class MergeSort {
 
         int length = Integer.parseInt(args[0]);
         int threshold = args.length == 2 ? Integer.parseInt(args[1]) : Integer.MAX_VALUE;
+        int seed = 0;
+        Stopwatch stopwatch = new Stopwatch();
 
+        int[] elements = new int[length];
+
+        Random r = new Random(seed);
+        for (int i = 0; i < length; i++) {
+            elements[i] = r.nextInt(length * 1000);
+        }
         System.out.println();
         System.out.println("Starting MergeSort with length = " + length + ", threshold = " + threshold);
         System.out.println();
 
-        MergeSort sort = new MergeSort(length, threshold);
-        System.out.println(Arrays.toString(sort.getElements()));
-        sort.sort();
-        System.out.println(Arrays.toString(sort.getElements()));
+        MergeSort sort = new MergeSort(threshold);
+        stopwatch.start();
+        int[] sorted = sort.mergeSort(elements, 0, length-1);
+        double sortTime = stopwatch.stop();
 
+        //System.out.println("Unsorted List: " + Arrays.toString(elements));
+        // System.out.println("Sorted List: " + Arrays.toString(sorted));
+        System.out.println(String.format("Needed calculationTime: %.10fms", sortTime));
     }
 }
